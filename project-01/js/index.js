04af6394d658e26751b3e55fa0f03ba5cbd0e524
Milton839 <miltonbiswas839@gmail.com>
2022-02-11 14:33:27 +0600

    project-01 complete

---
+++ project-01/js/index.js
@@ -0,0 +1,17 @@
+const body = document.getElementById("totalBody");
+
+const btn = document.getElementById("btn");
+
+const title = document.getElementById("title");
+
+btn.addEventListener("click", function(){
+    title.innerText = randomNumber();
+    body.style.backgroundColor = randomNumber();
+})
+
+function randomNumber(){
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    return `rgb(${red}, ${green}, ${blue})`
+}
